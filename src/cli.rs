2ddc97679cb1b0c0953afef1c2daed818230a890
Shringe <git@nsaria.com>
2025-09-15 18:27:06 -0500

    Working server and cli. No gui client yet. v1.0.0

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,26 @@
-use clap::{Parser, ValueEnum};
-
-#[derive(Clone, Debug, ValueEnum)]
-pub enum Mode {
-    Server,
-    Client,
-}
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(value_enum)]
+    #[command(subcommand)]
     pub mode: Mode,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Mode {
+    /// Runs the autoclicker. The default hotkey to toggle the clicker is F5. May need to run as root
+    Server {
+        /// Path of the device to listen to hotkeys from
+        #[arg(long)]
+        device: PathBuf,
+
+        /// Time to sleep in between clicks
+        #[arg(long, default_value_t = 50)]
+        interval: u64,
+    },
+    Client,
+    /// Lists input devices. May need root to see all devices.
+    List,
 }
