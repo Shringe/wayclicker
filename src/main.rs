1f888af394e3e0158312416bb2e0a0b0781a8616
Shringe <dashingkoso@gmail.com>
2025-09-15 16:24:14 -0500

    functional clicks

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,57 @@
 mod cli;
-use std::{process::exit, thread, time};
+mod server;
 
+use std::{thread::sleep, time::Duration};
+
+use crate::server::Server;
 use clap::Parser;
-// Uinput is used for send the inputs
-use uinput::event::{
-    controller::Mouse::{Left, Right},
-    keyboard::Key,
-    keyboard::Keyboard::All,
-};
 
-// Evdev is used for detecting keyboard input globally
-use evdev::{Device, KeyCode};
+// fn start_clicker() {
+//     let mut autoclicker = false; // Autoclicker toggle variable.
+//     let mut clicker = Clicker::new(); // Clicker object.
+//     let mut evdevdevice = get_evdev_device(); // Evdev device.
+//
+//     // Last frame's pressed keys.
+//     let mut lastkeys = vec![];
+//
+//     loop {
+//         // Get all currently pressed keys.
+//         let keypresses = evdevdevice
+//             .get_key_state()
+//             .unwrap()
+//             .iter()
+//             .collect::<Vec<KeyCode>>();
+//
+//         // Check for specific key presses
+//         for k in &keypresses {
+//             if k.0 == 62 && !lastkeys.contains(k) {
+//                 autoclicker = !autoclicker;
+//             }
+//         }
+//
+//         // Update lastkeys.
+//         lastkeys = keypresses;
+//
+//         // Repeatedly click when the autoclicker is on.
+//         if autoclicker {
+//             clicker.click(0);
+//             thread::sleep(time::Duration::from_millis(50));
+//         }
+//     }
+// }
 
-// Object oriented because it's easier for managing the device variable.
-struct Clicker {
-    device: uinput::device::Device,
-}
-impl Clicker {
-    // Creates clicker.
-    fn new() -> Clicker {
-        let device = uinput::default()
-            .unwrap()
-            .name("device")
-            .unwrap()
-            .event(Left)
-            .unwrap()
-            .event(All)
-            .unwrap()
-            .create()
-            .unwrap();
-
-        Clicker { device }
-    }
-    // Sends click.
-    fn click(&mut self, click_type: u8) {
-        match click_type {
-            0 => {
-                // Left Click.
-                self.device.send(Left, 1).unwrap();
-                self.device.synchronize().unwrap();
-                self.device.send(Left, 0).unwrap();
-                self.device.synchronize().unwrap();
-            }
-            1 => {
-                // Right Click.
-                self.device.send(Right, 1).unwrap();
-                self.device.synchronize().unwrap();
-                self.device.send(Right, 0).unwrap();
-                self.device.synchronize().unwrap();
-            }
-            2 => {
-                // Space Bar.
-                self.device.click(&Key::Space).unwrap();
-                self.device.synchronize().unwrap();
-            }
-            _ => { /* Only here so that the compiler doesn't get mad at me. */ }
-        }
-    }
-}
-
-// Get device for evdev.
-fn get_evdev_device() -> Device {
-    // Get all possible devices.
-    let mut devices = evdev::enumerate().map(|t| t.1).collect::<Vec<_>>();
-    devices.reverse();
-
-    // Check for any devices related to the keyboard.
-    let mut keyid = 255;
-    for (i, d) in devices.iter().enumerate() {
-        if d.name().unwrap_or("").to_lowercase().contains("keyboard") {
-            keyid = i;
-        }
-    }
-    // If no keyboard is found, crash.
-    if keyid == 255 {
-        eprintln!("No keyboard device found.");
-        exit(1);
-    }
-
-    // Return the keyboard device.
-    devices.into_iter().nth(keyid).unwrap()
-}
-
-fn start_clicker() {
-    let mut autoclicker = false; // Autoclicker toggle variable.
-    let mut clicker = Clicker::new(); // Clicker object.
-    let mut evdevdevice = get_evdev_device(); // Evdev device.
-
-    // Last frame's pressed keys.
-    let mut lastkeys = vec![];
-
-    loop {
-        // Get all currently pressed keys.
-        let keypresses = evdevdevice
-            .get_key_state()
-            .unwrap()
-            .iter()
-            .collect::<Vec<KeyCode>>();
-
-        // Check for specific key presses
-        for k in &keypresses {
-            if k.0 == 62 && !lastkeys.contains(k) {
-                autoclicker = !autoclicker;
-            }
-        }
-
-        // Update lastkeys.
-        lastkeys = keypresses;
-
-        // Repeatedly click when the autoclicker is on.
-        if autoclicker {
-            clicker.click(0);
-            thread::sleep(time::Duration::from_millis(50));
-        }
-    }
+fn start_server() {
+    let mut server = Server::new();
+    let pause = Duration::from_millis(500);
+    sleep(pause);
+    let _ = server.click(0);
 }
 
 fn main() {
     let args = cli::Args::parse();
 
     match args.mode {
-        cli::Mode::Server => todo!(),
+        cli::Mode::Server => start_server(),
         cli::Mode::Client => todo!(),
     }
 }
