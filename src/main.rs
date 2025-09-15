9916f8cc66863e1202d07030d7231c13e88609d7
Shringe <git@nsaria.com>
2025-09-15 16:08:58 -0500

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{thread, time, process::exit};
+mod cli;
+use std::{process::exit, thread, time};
 
+use clap::Parser;
 // Uinput is used for send the inputs
 use uinput::event::{
     controller::Mouse::{Left, Right},
@@ -17,15 +19,18 @@ struct Clicker {
 impl Clicker {
     // Creates clicker.
     fn new() -> Clicker {
-        let device = uinput::default().unwrap()
-            .name("device").unwrap()
-            .event(Left).unwrap()
-            .event(All).unwrap()
-            .create().unwrap();
+        let device = uinput::default()
+            .unwrap()
+            .name("device")
+            .unwrap()
+            .event(Left)
+            .unwrap()
+            .event(All)
+            .unwrap()
+            .create()
+            .unwrap();
 
-        Clicker {
-            device
-        }
+        Clicker { device }
     }
     // Sends click.
     fn click(&mut self, click_type: u8) {
@@ -33,14 +38,14 @@ impl Clicker {
             0 => {
                 // Left Click.
                 self.device.send(Left, 1).unwrap();
-                self.device.synchronize().unwrap(); 
+                self.device.synchronize().unwrap();
                 self.device.send(Left, 0).unwrap();
                 self.device.synchronize().unwrap();
             }
             1 => {
                 // Right Click.
                 self.device.send(Right, 1).unwrap();
-                self.device.synchronize().unwrap(); 
+                self.device.synchronize().unwrap();
                 self.device.send(Right, 0).unwrap();
                 self.device.synchronize().unwrap();
             }
@@ -59,7 +64,7 @@ fn get_evdev_device() -> Device {
     // Get all possible devices.
     let mut devices = evdev::enumerate().map(|t| t.1).collect::<Vec<_>>();
     devices.reverse();
-    
+
     // Check for any devices related to the keyboard.
     let mut keyid = 255;
     for (i, d) in devices.iter().enumerate() {
@@ -72,30 +77,34 @@ fn get_evdev_device() -> Device {
         eprintln!("No keyboard device found.");
         exit(1);
     }
-    
+
     // Return the keyboard device.
     devices.into_iter().nth(keyid).unwrap()
 }
 
-fn main() {
-    let mut autoclicker = false;              // Autoclicker toggle variable.
-    let mut clicker = Clicker::new();         // Clicker object.
+fn start_clicker() {
+    let mut autoclicker = false; // Autoclicker toggle variable.
+    let mut clicker = Clicker::new(); // Clicker object.
     let mut evdevdevice = get_evdev_device(); // Evdev device.
-    
+
     // Last frame's pressed keys.
     let mut lastkeys = vec![];
 
     loop {
         // Get all currently pressed keys.
-        let keypresses = evdevdevice.get_key_state().unwrap().iter().collect::<Vec<KeyCode>>();
-        
+        let keypresses = evdevdevice
+            .get_key_state()
+            .unwrap()
+            .iter()
+            .collect::<Vec<KeyCode>>();
+
         // Check for specific key presses
         for k in &keypresses {
             if k.0 == 62 && !lastkeys.contains(k) {
                 autoclicker = !autoclicker;
             }
         }
-        
+
         // Update lastkeys.
         lastkeys = keypresses;
 
@@ -104,5 +113,14 @@ fn main() {
             clicker.click(0);
             thread::sleep(time::Duration::from_millis(50));
         }
+    }
+}
+
+fn main() {
+    let args = cli::Args::parse();
+
+    match args.mode {
+        cli::Mode::Server => todo!(),
+        cli::Mode::Client => todo!(),
     }
 }
