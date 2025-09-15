f98d25c1dd903d6b4799a9fcbfaba43b299f0a31
Shringe <dashingkoso@gmail.com>
2025-09-15 18:27:06 -0500

    Working server and cli. No gui client yet. v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,31 @@
 mod cli;
 mod server;
 
-use std::{thread::sleep, time::Duration};
+use std::time::Duration;
 
 use crate::server::Server;
 use clap::Parser;
-
-// fn start_clicker() {
-//     let mut autoclicker = false; // Autoclicker toggle variable.
-//     let mut clicker = Clicker::new(); // Clicker object.
-//     let mut evdevdevice = get_evdev_device(); // Evdev device.
-//
-//     // Last frame's pressed keys.
-//     let mut lastkeys = vec![];
-//
-//     loop {
-//         // Get all currently pressed keys.
-//         let keypresses = evdevdevice
-//             .get_key_state()
-//             .unwrap()
-//             .iter()
-//             .collect::<Vec<KeyCode>>();
-//
-//         // Check for specific key presses
-//         for k in &keypresses {
-//             if k.0 == 62 && !lastkeys.contains(k) {
-//                 autoclicker = !autoclicker;
-//             }
-//         }
-//
-//         // Update lastkeys.
-//         lastkeys = keypresses;
-//
-//         // Repeatedly click when the autoclicker is on.
-//         if autoclicker {
-//             clicker.click(0);
-//             thread::sleep(time::Duration::from_millis(50));
-//         }
-//     }
-// }
-
-fn start_server() {
-    let mut server = Server::default().expect("Failed to get clicker");
-    let pause = Duration::from_millis(500);
-    sleep(pause);
-    let _ = server.click();
-}
+use evdev::{Device, KeyCode};
 
 fn main() {
     let args = cli::Args::parse();
 
     match args.mode {
-        cli::Mode::Server => start_server(),
+        cli::Mode::Server { device, interval } => {
+            let listenor = Device::open(device).unwrap();
+            let interval = Duration::from_millis(interval);
+            let hotkey = KeyCode::KEY_F5;
+            let mut server =
+                Server::new(listenor, interval, hotkey).expect("Failed to get create server");
+            server.run();
+        }
+
         cli::Mode::Client => todo!(),
+
+        cli::Mode::List => {
+            for (path, device) in evdev::enumerate() {
+                println!("{:?}: {}", path, device.name().unwrap_or("<error>"));
+            }
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,75 +1,77 @@
-use std::{error::Error, process::exit, thread, time::Duration};
+use std::{error::Error, thread, time::Duration};
 
 // Uinput is used for send the inputs
 use uinput::event::controller::Mouse;
 
 // Evdev is used for detecting keyboard input globally
-use evdev::Device;
+use evdev::KeyCode;
 
-// Object oriented because it's easier for managing the device variable.
 pub struct Server {
-    device: uinput::device::Device,
-    /// Amount of time to sleep in between clicks
+    listenor: evdev::Device,
+    clicker: uinput::device::Device,
+    hotkey: KeyCode,
     interval: Duration,
-    active: bool,
 }
 
 impl Server {
     // Creates the server and clicker
-    pub fn default() -> Result<Self, Box<dyn Error>> {
-        let device = uinput::default()?
+    pub fn new(
+        listenor: evdev::Device,
+        interval: Duration,
+        hotkey: KeyCode,
+    ) -> Result<Self, Box<dyn Error>> {
+        let clicker = uinput::default()?
             .name("device")?
             .event(Mouse::Left)?
             .create()?;
 
         Ok(Self {
-            device,
-            interval: Duration::from_millis(50),
-            active: false,
+            listenor,
+            clicker,
+            hotkey,
+            interval,
         })
     }
 
     /// Runs the server loop
     pub fn run(&mut self) {
-        todo!();
+        println!("Server ready");
+        let mut lastkeys = Vec::new();
+        let mut active = false;
         loop {
-            if self.active {
-                self.click();
+            let keypresses = self
+                .listenor
+                .get_key_state()
+                .unwrap()
+                .iter()
+                .collect::<Vec<KeyCode>>();
+
+            // Check for specific key presses
+            for k in &keypresses {
+                if *k == self.hotkey && !lastkeys.contains(k) {
+                    active = !active;
+                    if active {
+                        println!("Enabled clicker");
+                    } else {
+                        println!("Disabled clicker");
+                    }
+                }
             }
 
+            if active {
+                let _ = self.click();
+            }
+
+            lastkeys = keypresses;
             thread::sleep(self.interval);
         }
     }
 
     // Sends a left click
     pub fn click(&mut self) -> Result<(), Box<dyn Error>> {
-        self.device.send(Mouse::Left, 1)?;
-        self.device.synchronize()?;
-        self.device.send(Mouse::Left, 0)?;
-        self.device.synchronize()?;
+        self.clicker.send(Mouse::Left, 1)?;
+        self.clicker.send(Mouse::Left, 0)?;
+        self.clicker.synchronize()?;
         Ok(())
     }
-}
-
-// Get device for evdev.
-fn get_evdev_device() -> Device {
-    // Get all possible devices.
-    let mut devices = evdev::enumerate().map(|t| t.1).collect::<Vec<_>>();
-    devices.reverse();
-
-    // Check for any devices related to the keyboard.
-    let mut keyid = 255;
-    for (i, d) in devices.iter().enumerate() {
-        if d.name().unwrap_or("").to_lowercase().contains("keyboard") {
-            keyid = i;
-        }
-    }
-    // If no keyboard is found, crash.
-    if keyid == 255 {
-        eprintln!("No keyboard device found.");
-        exit(1);
-    }
-
-    // Return the keyboard device.
-    devices.into_iter().nth(keyid).unwrap()
 }
