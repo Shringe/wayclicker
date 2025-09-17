de30eef6e1f5275916ee3ed62f68422919d56a6d
Shringe <git@nsaria.com>
2025-09-17 09:43:57 -0500

    extrapolated hotkey to stuct

--- src/server.rs
+++ src/server.rs
@@ -6,10 +6,51 @@ use uinput::event::controller::Mouse;
 // Evdev is used for detecting keyboard input globally
 use evdev::KeyCode;
 
-pub struct Server {
+struct HotKey {
     listenor: evdev::Device,
+    keybind: KeyCode,
+    lastkeys: Vec<KeyCode>,
+    active: bool,
+}
+
+impl HotKey {
+    fn new(listenor: evdev::Device, keybind: KeyCode) -> Self {
+        Self {
+            listenor,
+            keybind,
+            lastkeys: Vec::new(),
+            active: false,
+        }
+    }
+
+    pub fn is_active(&mut self) -> bool {
+        let keypresses = self
+            .listenor
+            .get_key_state()
+            .unwrap()
+            .iter()
+            .collect::<Vec<KeyCode>>();
+
+        // Check for specific key presses
+        for k in &keypresses {
+            if *k == self.keybind && !self.lastkeys.contains(k) {
+                self.active = !self.active;
+                if self.active {
+                    println!("Enabled clicker");
+                } else {
+                    println!("Disabled clicker");
+                }
+            }
+        }
+
+        self.lastkeys = keypresses;
+        self.active
+    }
+}
+
+pub struct Server {
     clicker: uinput::device::Device,
-    hotkey: KeyCode,
+    hotkey: HotKey,
     interval: Duration,
 }
 
@@ -18,15 +59,15 @@ impl Server {
     pub fn new(
         listenor: evdev::Device,
         interval: Duration,
-        hotkey: KeyCode,
+        keybind: KeyCode,
     ) -> Result<Self, Box<dyn Error>> {
         let clicker = uinput::default()?
             .name("device")?
             .event(Mouse::Left)?
             .create()?;
 
+        let hotkey = HotKey::new(listenor, keybind);
         Ok(Self {
-            listenor,
             clicker,
             hotkey,
             interval,
@@ -36,33 +77,13 @@ impl Server {
     /// Runs the server loop
     pub fn run(&mut self) {
         println!("Server ready");
-        let mut lastkeys = Vec::new();
-        let mut active = false;
         loop {
-            let keypresses = self
-                .listenor
-                .get_key_state()
-                .unwrap()
-                .iter()
-                .collect::<Vec<KeyCode>>();
-
-            // Check for specific key presses
-            for k in &keypresses {
-                if *k == self.hotkey && !lastkeys.contains(k) {
-                    active = !active;
-                    if active {
-                        println!("Enabled clicker");
-                    } else {
-                        println!("Disabled clicker");
-                    }
-                }
-            }
+            let active = self.hotkey.is_active();
 
             if active {
                 let _ = self.click();
             }
 
-            lastkeys = keypresses;
             thread::sleep(self.interval);
         }
     }
