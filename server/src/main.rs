ba32984f896ef1984f4e41a7d0f470cd685c3783
Shringe <dashingkoso@gmail.com>
2025-10-29 22:54:57 -0500

    added old cli file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,14 @@
+mod cli;
 mod hotkey;
 mod server;
+
+use clap::Parser;
 
 use crate::server::Server;
 
 fn main() {
     env_logger::init();
+    let args = cli::Args::parse();
     let mut server = Server::new().expect("Failed to initialize server");
     server.run();
 }
