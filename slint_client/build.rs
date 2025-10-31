9ff286e19ed6559a339942b243af726b62d8846d
Shringe <dashingkoso@gmail.com>
2025-10-31 16:00:34 -0500

    removed non-qt backend that threw build errors on cargo check

--- slint_client/build.rs
+++ slint_client/build.rs
@@ -1,6 +1,6 @@
-// use slint_build::CompilerConfiguration;
+use slint_build::CompilerConfiguration;
 
 fn main() {
-    // let config = CompilerConfiguration::new().with_style("natvi".to_string());
-    slint_build::compile("ui.slint").unwrap();
+    let config = CompilerConfiguration::new().with_style("qt".to_string());
+    slint_build::compile_with_config("ui.slint", config).unwrap();
 }
