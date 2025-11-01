e58be26877fadc47d37c81e352478be46c1d926c
Shringe <dashingkoso@gmail.com>
2025-11-01 16:23:46 -0500

    moved slint main file

--- slint_client/build.rs
+++ slint_client/build.rs
@@ -2,5 +2,5 @@ use slint_build::CompilerConfiguration;
 
 fn main() {
     let config = CompilerConfiguration::new().with_style("qt".to_string());
-    slint_build::compile_with_config("ui.slint", config).unwrap();
+    slint_build::compile_with_config("ui/main.slint", config).unwrap();
 }
